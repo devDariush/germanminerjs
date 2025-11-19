875845954c9cf47878f182741b08442df304b606
Dariush <118191394+drshk@users.noreply.github.com>
2025-11-19 01:43:01 +0100

    chore: add more exports in mod.ts for ClientOptions and custom errors

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,12 @@
-import { GMClient } from "./client.ts";
+import { type ClientOptions, GMClient } from "./client.ts";
+import { ApiError } from "./common/errors/api-error.ts";
+import { GmClientError } from "./common/errors/client-error.ts";
+import { LimitReachedError } from "./common/errors/limit-reached-error.ts";
 
-export { GMClient };
+export {
+  ApiError,
+  type ClientOptions,
+  GMClient,
+  GmClientError,
+  LimitReachedError,
+};
