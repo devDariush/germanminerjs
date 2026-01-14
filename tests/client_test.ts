3e5a6d53f224d1e02f1cb1bb0deadf59d0e47009
Dariush Komeili <dkomeili@proton.me>
2026-01-14 15:19:48 +0100

    Test

---
+++ tests/client_test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { stub } from "@std/testing/mock";
+
+describe("Test", () => {
+  it("should be a test", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
